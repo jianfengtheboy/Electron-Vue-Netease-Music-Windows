64869a7bccff67eab103f68a554857145e08226b
孙剑锋 <jianfengtheboy@163.com>
2020-04-20 22:43:13 +0800

    没有更多组件

--- src/renderer/components/Common/infiniteNoMore.vue
+++ src/renderer/components/Common/infiniteNoMore.vue
@@ -3,21 +3,22 @@
  * @LastEditors: SunJianFeng
  * @Email: jianfengtheboy@163.com
  * @Date: 2020-04-05 16:01:45
- * @LastEditTime: 2020-04-18 23:08:17
- * @Description:
+ * @LastEditTime: 2020-04-20 22:42:59
+ * @Description: 没有更多组件
  -->
 <template>
-  <div>
-
-  </div>
+  <div class="no-more">没有更多了~</div>
 </template>
 
 <script>
 export default {
-
+  name: 'infiniteNoMore'
 }
 </script>
 
-<style>
-
+<style scoped>
+.no-more {
+  margin: 15px 0;
+  text-align: center;
+}
 </style>
