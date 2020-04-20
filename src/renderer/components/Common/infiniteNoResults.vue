c68d4087456655471d9be0f86a213772202533e0
孙剑锋 <jianfengtheboy@163.com>
2020-04-20 22:44:34 +0800

    暂无请求结果组件

--- src/renderer/components/Common/infiniteNoResults.vue
+++ src/renderer/components/Common/infiniteNoResults.vue
@@ -3,21 +3,22 @@
  * @LastEditors: SunJianFeng
  * @Email: jianfengtheboy@163.com
  * @Date: 2020-04-05 16:01:45
- * @LastEditTime: 2020-04-18 23:08:29
- * @Description:
+ * @LastEditTime: 2020-04-20 22:44:23
+ * @Description: 暂无请求结果组件
  -->
 <template>
-  <div>
-
-  </div>
+  <div class="no-result">暂无请求结果~</div>
 </template>
 
 <script>
 export default {
-
+  name: 'infiniteNoResults'
 }
 </script>
 
-<style>
-
+<style scoped>
+.no-result {
+  margin: 15px 0;
+  text-align: center;
+}
 </style>
