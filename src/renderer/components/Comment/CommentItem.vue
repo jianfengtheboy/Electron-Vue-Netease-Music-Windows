c26124c7922b06b65fde37d27f53e6df95e26869
孙剑锋 <jianfengtheboy@163.com>
2020-04-20 21:46:13 +0800

    player

--- src/renderer/components/Comment/CommentItem.vue
+++ src/renderer/components/Comment/CommentItem.vue
@@ -3,21 +3,78 @@
  * @LastEditors: SunJianFeng
  * @Email: jianfengtheboy@163.com
  * @Date: 2020-04-09 22:27:53
- * @LastEditTime: 2020-04-18 23:06:11
+ * @LastEditTime: 2020-04-20 21:43:27
  * @Description: CommentItem
  -->
 <template>
-  <div>
+  <a-comment class="comment">
+    <template slot="actions">
+      <span>
+        <a-icon type="like" :theme="comment.liked?'filled':'outlined'" />
+        <span v-if="comment.likedCount">({{comment.likedCount}})</span>
+      </span>
+      <span>分享</span>
+      <span>回复</span>
+    </template>
+    <router-link :to="`/user?id=${comment.user.userId}`" slot="author">{{comment.user.nickname}}:</router-link>
+    <router-link :to="`/user?id=${comment.user.userId}`" slot="avatar">
+      <img v-lazy="`${comment.user.avatarUrl}?param=32y32`" class="avatar" />
+    </router-link>
 
-  </div>
+    <span slot="datetime">{{moment(comment.time).format('YYYY年MM月DD日 HH:mm')}}</span>
+    <div slot="content">
+      <p>{{comment.content}}</p>
+      <template v-if="comment.beReplied && comment.beReplied.length">
+        <div
+          class="beReplied"
+          v-for="(reply, index) in comment.beReplied"
+          :key="`${reply.beRepliedCommentId}_${index}`"
+        >
+          <router-link :to="`/user?id=${reply.user.userId}`">@{{reply.user.nickname}}</router-link>
+          <span>{{reply.content}}</span>
+        </div>
+      </template>
+    </div>
+  </a-comment>
 </template>
 
 <script>
-export default {
+import moment from 'moment'
 
+export default {
+  data () {
+    return {
+      moment
+    }
+  },
+  props: {
+    comment: {
+      type: Object
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.comment-wrapper dt {
+  padding-bottom: 15px;
+}
+.comment-wrapper dt {
+  border-bottom: 1px solid #e6e6e6;
+}
+.comment-wrapper dd:not(:last-child) {
+  border-bottom: 1px solid #e6e6e6;
+}
+.comment >>> .ant-comment-actions {
+  float: right;
+  margin: 0;
+}
+.comment a {
+  color: #006fe3;
+}
+.beReplied {
+  background: rgba(232, 232, 232, 0.5);
+  padding: 3px 6px;
+  border-radius: 3px;
+}
 </style>
--- src/renderer/components/Comment/index.vue
+++ src/renderer/components/Comment/index.vue
@@ -3,21 +3,49 @@
  * @LastEditors: SunJianFeng
  * @Email: jianfengtheboy@163.com
  * @Date: 2020-04-09 22:28:25
- * @LastEditTime: 2020-04-09 22:28:33
+ * @LastEditTime: 2020-04-20 21:41:58
  * @Description:
  -->
 <template>
   <div>
-
+    <div class="comment-wrapper" v-if="commentData">
+      <dl v-if="commentData.hotComments.length">
+        <dt>热门评论</dt>
+        <dd v-for="(comment, index) in commentData.hotComments" :key="`hot_${comment.commentId}_${index}`">
+          <comment-item :comment="comment" />
+        </dd>
+      </dl>
+      <dl v-if="commentData.topComments.length">
+        <dt>精彩评论</dt>
+        <dd v-for="(comment, index) in commentData.topComments" :key="`top_${comment.commentId}_${index}`">
+          <comment-item :comment="comment" />
+        </dd>
+      </dl>
+      <dl v-if="commentData.comments.length">
+        <dt>全部评论</dt>
+        <dd v-for="(comment, index) in commentData.comments" :key="`all_${comment.commentId}_${index}`">
+          <comment-item :comment="comment" />
+        </dd>
+      </dl>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import CommentItem from './CommentItem'
+import Loading from '@/components/Common/loading'
 
+export default {
+  props: {
+    commentData: {
+      type: Object,
+      default () {
+        return null
+      }
+    }
+  },
+  components: {
+    Loading, CommentItem
+  }
 }
 </script>
-
-<style>
-
-</style>
