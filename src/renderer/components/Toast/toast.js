1a36be2fdf47c973b47f2d7d87f7070bfec49b99
孙剑锋 <jianfengtheboy@163.com>
2020-04-20 23:12:13 +0800

    通知弹框

--- src/renderer/components/Toast/toast.js
+++ src/renderer/components/Toast/toast.js
@@ -3,6 +3,24 @@
  * @LastEditors: SunJianFeng
  * @Email: jianfengtheboy@163.com
  * @Date: 2020-04-05 16:01:45
- * @LastEditTime: 2020-04-18 23:12:45
- * @Description:
+ * @LastEditTime: 2020-04-20 23:11:53
+ * @Description: 通知弹框
  */
+import Notification from './../base/notification'
+let toastInstance
+
+function getToastInstance () {
+  toastInstance = toastInstance || Notification.newInstance()
+  return toastInstance
+}
+
+const notice = function ({ content = '提示', icon = '', duration = 3 }) {
+  let instance = getToastInstance()
+  instance.notice({
+    content,
+    icon,
+    duration
+  })
+}
+
+export default notice
