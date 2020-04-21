da6a6a2f910374eceedbe9e9ed67f49c23853be4
孙剑锋 <jianfengtheboy@163.com>
2020-04-21 21:43:18 +0800

    private

--- src/renderer/views/Home/components/private.vue
+++ src/renderer/views/Home/components/private.vue
@@ -3,21 +3,54 @@
  * @LastEditors: SunJianFeng
  * @Email: jianfengtheboy@163.com
  * @Date: 2020-04-05 16:01:45
- * @LastEditTime: 2020-04-18 22:46:29
- * @Description:
+ * @LastEditTime: 2020-04-21 21:42:53
+ * @Description: private
  -->
 <template>
-  <div>
-
+  <div class="mv">
+    <ul class="list">
+      <list-item
+        item-type="mv"
+        :ratio="0.56"
+        class="list-item"
+        v-for="item in list"
+        :item="item"
+        :key="item.id"
+      />
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
+import listItem from '@/components/Common/list-item'
 
+export default {
+  name: 'mv',
+  props: {
+    list: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  components: {
+    listItem
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+@gutter: 1%;
+.list {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  margin: 0 -@gutter;
+  .list-item {
+    width: 23%;
+    margin-left: @gutter;
+    margin-right: @gutter;
+  }
+}
 </style>
