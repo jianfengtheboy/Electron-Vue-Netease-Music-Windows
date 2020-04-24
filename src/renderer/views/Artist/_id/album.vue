6a5914e7b639be734496aff1b00cf61226b7a81e
孙剑锋 <jianfengtheboy@163.com>
2020-04-24 21:53:12 +0800

    💽专辑💽

--- src/renderer/views/Artist/_id/album.vue
+++ src/renderer/views/Artist/_id/album.vue
@@ -3,21 +3,74 @@
  * @LastEditors: SunJianFeng
  * @Email: jianfengtheboy@163.com
  * @Date: 2020-04-05 16:01:45
- * @LastEditTime: 2020-04-18 22:41:08
- * @Description:
+ * @LastEditTime: 2020-04-24 21:50:34
+ * @Description: 💽专辑💽
  -->
 <template>
-  <div>
-
+  <div class="artist-album">
+    <ul class="albums">
+      <album-item v-for="album in albums" :album="album" :key="album.id" />
+    </ul>
+    <infinite-loading
+      forceUseInfiniteWrapper=".ant-layout-content"
+      :identifier="infiniteId"
+      @infinite="loadmore"
+    />
   </div>
 </template>
 
 <script>
-export default {
+import albumItem from '@/components/Common/album-item'
+import { getArtistAlbum } from '@/api/artist'
+import { normalSong } from '@/utils/song'
 
+export default {
+  name: 'artist-id-album',
+  data () {
+    return {
+      albums: [],
+      limit: 50,
+      offset: 0,
+      infiniteId: +new Date()
+    }
+  },
+  components: {
+    albumItem
+  },
+  activated () {
+    this.offset = 0
+    this.albums = []
+    this.infiniteId++
+  },
+  methods: {
+    async loadmore ($state) {
+      let params = {
+        id: this.$route.params.id,
+        limit: this.limit,
+        offset: this.offset
+      }
+      try {
+        let { hotAlbums, more } = await getArtistAlbum(params)
+        this.albums = this.albums.concat(hotAlbums)
+        $state.loaded()
+        if ( more ) {
+          this.offset += this.limit
+        } else {
+          $state.complete()
+        }
+      } catch ( error ) {
+        $state.error()
+      }
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+@import "./../../../styles/mixins";
 
+.albums {
+  .grid-layout(15px, 141px);
+  padding: 15px;
+}
 </style>
