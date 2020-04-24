93c743381675d5d12fc55cfd8c13db27e230982a
孙剑锋 <jianfengtheboy@163.com>
2020-04-24 22:43:21 +0800

    歌手详情

--- src/renderer/views/Artist/_id/desc.vue
+++ src/renderer/views/Artist/_id/desc.vue
@@ -3,21 +3,65 @@
  * @LastEditors: SunJianFeng
  * @Email: jianfengtheboy@163.com
  * @Date: 2020-04-05 16:01:45
- * @LastEditTime: 2020-04-18 22:41:24
- * @Description:
+ * @LastEditTime: 2020-04-24 22:42:13
+ * @Description: 歌手详情
  -->
 <template>
-  <div>
-
-  </div>
+  <section class="desc">
+    <dl>
+      <dt>简介</dt>
+      <dd>{{briefDesc}}</dd>
+    </dl>
+    <dl v-for="item in introduction" :key="item.id">
+      <dt>{{item.ti}}</dt>
+      <dd>{{item.txt}}</dd>
+    </dl>
+    <dl v-for="(item, index) in topicData" :key="index">
+      <dt>{{item.mainTitle}}</dt>
+      <dd v-for="(con, i) in item.topic.content" :key="i" v-html="con.content"></dd>
+    </dl>
+  </section>
 </template>
 
 <script>
-export default {
+import { getArtistDesc } from '@/api/artist'
 
+export default {
+  name: 'artist-id-desc',
+  data () {
+    return {
+      introduction: [],
+      briefDesc: '',
+      topicData: [],
+      loading: false
+    }
+  },
+  activated () {
+    this._getArtistDesc()
+  },
+  methods: {
+    async _getArtistDesc () {
+      this.loading = true
+      let { id } = this.$route.params
+      let { introduction, briefDesc, topicData } = await getArtistDesc({ id })
+      this.introduction = introduction
+      this.briefDesc = briefDesc
+      this.topicData = topicData
+      this.loading = false
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.desc {
+  padding: 20px;
+  dl {
+    margin-bottom: 20px;
+  }
+  dd {
+    line-height: 1.4;
+    color: #777;
+  }
+}
 </style>
