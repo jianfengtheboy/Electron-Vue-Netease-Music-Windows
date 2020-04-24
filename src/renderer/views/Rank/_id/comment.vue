3d4a31dd4784e980197aa735eb6a8f7a60e2ffcf
孙剑锋 <jianfengtheboy@163.com>
2020-04-24 20:31:26 +0800

    评论

--- src/renderer/views/Rank/_id/comment.vue
+++ src/renderer/views/Rank/_id/comment.vue
@@ -3,21 +3,88 @@
  * @LastEditors: SunJianFeng
  * @Email: jianfengtheboy@163.com
  * @Date: 2020-04-05 16:01:45
- * @LastEditTime: 2020-04-18 22:48:48
- * @Description:
+ * @LastEditTime: 2020-04-24 20:30:25
+ * @Description: 评论
  -->
 <template>
-  <div>
-
+  <div class="comment-wrapper">
+    <div class="comment" v-if="commentData">
+      <comment :commentData="commentData"></comment>
+    </div>
+    <infinite-loading
+      forceUseInfiniteWrapper=".ant-layout-content"
+      :identifier="infiniteId"
+      @infinite="loadmore"
+    />
   </div>
 </template>
 
 <script>
-export default {
+import Comment from '@/components/Comment/index.vue'
+import { getPlaylistComment } from '@/api/comment'
 
+export default {
+  name: 'rank-id-comment',
+  data () {
+    return {
+      commentData: null,
+      limit: 20,
+      offset: 0,
+      infiniteId: +new Date(),
+      refresh: false
+    }
+  },
+  props: {
+    rank: {}
+  },
+  activated () {
+    this.offset = 0
+    this.commentData = null
+    this.infiniteId++
+  },
+  components: {
+    Comment
+  },
+  methods: {
+    async loadmore ($state) {
+      let id = this.$route.params.id
+      let res = await getPlaylistComment(id, this.limit, this.offset)
+      if (res.comments.length) {
+        if (this.commentData) {
+          this.commentData.comments.push(...res.comments)
+        } else {
+          this.commentData = res
+        }
+      }
+      $state.loaded()
+      if (res.more) {
+        this.offset += this.limit
+      } else {
+        $state.complete()
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.comment-wrapper {
+  padding: 20px;
+}
+.comment-wrapper dt {
+  padding-bottom: 15px;
+}
+.comment-wrapper dt {
+  border-bottom: 1px solid #f3f5f9;
+}
+.comment-wrapper dd:not(:last-child) {
+  border-bottom: 1px solid #f3f5f9;
+}
+.comment >>> .ant-comment-actions {
+  float: right;
+  margin: 0;
+}
+.comment a {
+  color: #006fe3;
+}
 </style>
