f6633e802e31833d8caf5355fcb0fe1e120dccb0
孙剑锋 <jianfengtheboy@163.com>
2020-04-25 18:50:34 +0800

    歌曲列表

--- src/renderer/views/Album/_id/songs.vue
+++ src/renderer/views/Album/_id/songs.vue
@@ -3,21 +3,51 @@
  * @LastEditors: SunJianFeng
  * @Email: jianfengtheboy@163.com
  * @Date: 2020-04-05 16:01:45
- * @LastEditTime: 2020-04-09 23:20:15
- * @Description:
+ * @LastEditTime: 2020-04-25 18:42:45
+ * @Description: 歌曲列表
  -->
 <template>
-  <div>
-
+  <div class="tracks">
+    <track-list :tracks="tracks" @dblclick="play" @download="download" />
   </div>
 </template>
 
 <script>
+import { getSongUrl, getLyric } from '@/api/song'
+import TrackList from '@/components/Common/track-list/index.js'
 export default {
-
+  name: 'album_id_songs',
+  data () {
+    return {
+      songUrl: '',
+      currentTime: 0,
+      buffered: 0
+    }
+  },
+  components: {
+    TrackList
+  },
+  props: {
+    tracks: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  methods: {
+    play (tracks, index) {
+      this.$store.dispatch('play/selectPlay', { tracks, index })
+    },
+    download (song) {
+      this.$store.dispatch('Download/download', song)
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.tracks {
+  margin-top: -1px;
+}
 </style>
