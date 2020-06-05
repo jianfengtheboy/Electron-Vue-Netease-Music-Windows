7303d60b8f4b7f1ba75120d3ef1262220f1a7765
孙剑锋 <jianfengtheboy@163.com>
2020-06-06 00:35:18 +0800

    我的音乐云盘

--- src/renderer/views/User/Cloud.vue
+++ src/renderer/views/User/Cloud.vue
@@ -3,21 +3,51 @@
  * @LastEditors: SunJianFeng
  * @Email: jianfengtheboy@163.com
  * @Date: 2020-04-05 16:01:45
- * @LastEditTime: 2020-04-18 22:54:47
- * @Description:
+ * @LastEditTime: 2020-06-06 00:33:02
+ * @Description: 我的音乐云盘
  -->
 <template>
-  <div>
-
-  </div>
+  <track-list :tracks="tracks" @dblclick="play" @download="download" />
 </template>
 
 <script>
-export default {
+import TrackList from '@/components/Common/track-list'
+import DebounceBtn from '@/components/Common/debounce-btn'
+import { getUserCloud } from '@/api/user'
+import { normalSong } from '@/utils/song'
+import { mapGetters } from 'vuex'
+import playMixin from '@/mixins/Play'
 
+export default {
+  name: 'cloud',
+  mixins: [
+    playMixin
+  ],
+  data () {
+    return {
+      tracks: []
+    }
+  },
+  components: {
+    TrackList,
+    DebounceBtn
+  },
+  computed: {
+    ...mapGetters('User', ['userId'])
+  },
+  activated () {
+    this._getUserCloud()
+  },
+  methods: {
+    _getUserCloud () {
+      getUserCloud(this.userId).then(res => {
+        this.tracks = res.data.map(song => {
+          return normalSong(song.simpleSong)
+        })
+      })
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style scoped></style>
